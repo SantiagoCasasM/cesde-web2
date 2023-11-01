42a19840977c76377a44621f67c1ac092383b297
Santiago Casas <50216041+SantiagoCasasM@users.noreply.github.com>
2023-11-01 06:46:36 -0500

    Add files via upload
    Contiene practica sobre hooks JavaScript

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Enumerador from "./components/Enumerador";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Putos todos</h1>
+      <Enumerador></Enumerador>
+    </div>
+  );
+}
+
+export default App;
