2281fe7d370b1052d5999e3a1a2891e925277d49
Santiago Casas <50216041+SantiagoCasasM@users.noreply.github.com>
2023-11-01 08:40:26 -0500

    Add files via upload
    Contiene ejemplo de como consumir API

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from "react";
-import Enumerador from "./components/Enumerador";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/todos/";
+  const [todos, setTodos] = useState();
+  const fetchApi = async () => {
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    setTodos(responseJSON);
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
     <div className="App">
-      <h1>Putos todos</h1>
-      <Enumerador></Enumerador>
+      Consumo de API
+      <ul>
+        {!todos
+          ? "Se esta cargando lka informacion de una API"
+          : todos.map((todo, index) => {
+              return (
+                <li>
+                  {todo.title}
+                  {todo.completed ? "- ok" : "- bad"}
+                </li>
+              );
+            })}
+      </ul>
     </div>
   );
 }
